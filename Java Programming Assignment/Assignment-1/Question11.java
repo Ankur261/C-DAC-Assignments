8e0e357d7de5e1d5b2f9e46a07451cb90965c312
Ankur Agrawal <68275567+Ankur261@users.noreply.github.com>
2025-03-06 08:05:53 +0530

    Java-Assignment-1

--- Java Programming Assignment/Assignment-1/Question11.java
+++ Java Programming Assignment/Assignment-1/Question11.java
@@ -21,7 +21,7 @@ class Question11 {
             largestNum = num1 ;
         }
         else if(num2 > num1 && num2 > num3) {
-            largestNum = num1 ;
+            largestNum = num2 ;
         }
         else {
             largestNum = num3 ;
