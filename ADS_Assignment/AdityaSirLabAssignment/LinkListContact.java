d69c1d9011cb46cd540a21620883cd7a2e9e9891
Ankur Agrawal <68275567+Ankur261@users.noreply.github.com>
2025-03-31 15:49:31 +0530

    assignment

--- ADS_Assignment/AdityaSirLabAssignment/LinkListContact.java
+++ ADS_Assignment/AdityaSirLabAssignment/LinkListContact.java
@@ -2,7 +2,6 @@ import java.util.* ;
 
 class CustomLinkList {
     Node head = null ;
-    Node tail = null ;
 
     public static class Node {
         String name ;
@@ -34,14 +33,22 @@ class CustomLinkList {
 
     }
 
-    // static Node removeContact(Node head, String Name) {
-
-    // }
+    static void removeContact(Node head, String name) {
+        // Node newHead = head ;
+        while(head.next != null ) {
+            if(head.next.name == name) {
+               head.next = head.next.next ;
+            }
+            head = head.next ;
+        }
+        System.out.println("Contact not found");
+    }
 
     static void searchByName(Node head, String name) {
         while(head != null ) {
             if(head.name == name) {
                 System.out.println(head.name+ ": " + head.number);
+                return ;
             }
             head = head.next ;
         }
@@ -54,6 +61,45 @@ class CustomLinkList {
             head = head.next ;
         }
     }
+
+    static void choiceList(Scanner sc,  CustomLinkList llist) {
+
+        System.out.println("Welcome to contact management: ");
+        System.out.println("Enter 1 to add a contact ");
+        System.out.println("Enter 2 to display all contacts ");
+        System.out.println("Enter 3 to search a contact ");
+        System.out.println("Enter 4 to delete a contact ");
+        int choice = sc.nextInt() ;
+
+        String name ;
+        String number;
+        
+        switch(choice) {
+            case 1 : 
+            System.out.println("Enter name of the contact ");
+            String name = sc.next() ;
+            System.out.println("Enter number of the contact ");
+            String number = sc.next() ;
+            llist.head = CustomLinkList.createContact(llist.head, name, number) ;
+            break ;
+            case 2 : 
+            CustomLinkList.displayAllContacts(llist.head) ;
+            break ;
+            case 3:
+            System.out.println("Enter name of the contact you want to search");
+            name = sc.nextLine() ;
+            llist.head = CustomLinkList.searchByName(llist.head, name) ;    
+            break ;
+            case 4:
+            System.out.println("Enter name of the contact you want to delete");
+            String name = sc.nextLine() ;
+            llist.head = CustomLinkList.searchByName(llist.head, name) ;    
+            break ;
+        }
+
+        choiceList(sc);
+
+    }
 }
 
 class LinkListContact {
@@ -62,9 +108,17 @@ class LinkListContact {
         Scanner sc = new Scanner(System.in) ;
         CustomLinkList llist = new CustomLinkList() ;
         llist.head = CustomLinkList.createContact(llist.head, "Ankur", "9876543210") ;
+        llist.head = CustomLinkList.createContact(llist.head, "Test User", "4234234234") ;
 
-        CustomLinkList.displayAllContacts(llist.head) ;
-        CustomLinkList.searchByName(llist.head, "Ankur") ;
+        CustomLinkList.choiceList(sc, llist) ;
+        
+       
+        // CustomLinkList.displayAllContacts(llist.head) ;
+        CustomLinkList.searchByName(llist.head, "Test User") ;
+        // CustomLinkList.removeContact(llist.head, "Ankur") ;
+
+
+        sc.close() ;
 
     }
 }
